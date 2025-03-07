923c1a173497251c9e16bf9786fcdf798249db4e
Muzuuu <muzzus453@gmail.com>
2025-03-08 00:13:51 +0530

    one more lec done

---
+++ javascriptpractic/1lec/3rd lec/index.js
@@ -0,0 +1,7 @@
+// let mydiv = document.querySelector("#mydiv");
+// let newele = document.createElement('span');
+// newele.textContent ='this is me muzammil';
+// mydiv.insertAdjacentElement('afterend',newele);
+// let parent = document.querySelector("#mydiv");
+// let child = document.querySelector('#fpara');
+// parent.removeChild(child);
