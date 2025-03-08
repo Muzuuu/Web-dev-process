b7bfde351ad176b8fdfd56d872c0f2d4395ea3d3
Muzuuu <muzzus453@gmail.com>
2025-03-09 01:13:29 +0530

    lec done

--- javascriptpractic/1lec/4th lec/index.js
+++ javascriptpractic/1lec/4th lec/index.js
@@ -15,14 +15,47 @@
 
 // let para = document.querySelectorAll('p');
 
-function alertpara() {
-    if(event.target.nodeName === 'span'){
-        alert("you have clicked on para:" + event.target.textContent);
-    }
+// function alertpara() {
+//     if(event.target.nodeName === 'span'){
+//         alert("you have clicked on para:" + event.target.textContent);
+//     }
     
-}
-// for(let i=0; i < para.length ; i++){
-//     para.addEventListener('click',alertpara);
 // }
-let mydiv = document.getElementById('wraper');
-document.addEventListener('click',alertpara);
+// // for(let i=0; i < para.length ; i++){
+// //     para.addEventListener('click',alertpara);
+// // }
+// let mydiv = document.getElementById('wraper');
+// document.addEventListener('click',alertpara);
+
+
+//reflow   repaint
+// // code 1 
+// const t1 =performance.now()
+// for(let i=0; i<=100 ;i++){
+//     let para = document.createElement('p');
+//     para.textContent ="this is para"+i;
+//     document.body.appendChild(para);
+// }
+// const t2 = performance.now();
+// console.log("total time by code 1:",(t2-t1));
+
+// // code 2 
+// const t3 =performance.now();
+// let mydiv = document.createElement('div');
+
+// for(let i=0; i<=100 ;i++){
+//     let para = document.createElement('p');
+//     para.textContent ="this is para"+i;
+//     mydiv.appendChild(para);
+// }
+// document.body.appendChild(para);
+// const t4 = performance.now();
+// console.log("total time by code 2:",(t4-t3));
+
+// let fragment =document.createDocumentFragment();
+// for(let i=0; i<=100 ;i++){
+//     let para = document.createElement('p');
+//     para.textContent ="this is para"+i;
+//     fragment.appendChild(para);
+// }
+// document.body.appendChild(fragment);
