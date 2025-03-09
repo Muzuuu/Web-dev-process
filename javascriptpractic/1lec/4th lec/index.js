2fa6abf9a33ca6ed09d63baed427634c0b392b76
Muzuuu <muzzus453@gmail.com>
2025-03-09 15:51:36 +0530

    lec done

--- javascriptpractic/1lec/4th lec/index.js
+++ javascriptpractic/1lec/4th lec/index.js
@@ -59,3 +59,32 @@
 //     fragment.appendChild(para);
 // }
 // document.body.appendChild(fragment);
+
+// // code 1 
+// const t1 =performance.now();
+// for(let i=0;i<=100;i++){
+//     let para = document.createElement('p');
+//     para.textContent ='para:'+i;
+//     document.body.appendChild(para);
+// }
+// const t2 =performance.now();
+// console.log('total time :'+(t2-t1));
+
+// // code 2 
+// const t3 = performance.now();
+// let mydiv = document.createElement('div');
+// for (let i=1 ;i<=100;i++){
+//     let para =document.createElement('p');
+//     para.textContent ="para:"+i;
+//     mydiv.appendChild(para);
+// }
+// document.body.appendChild(mydiv);
+// const t4 =performance.now();
+// console.log('total time :'+(t4-t3));
+
+// let fpromis =new Promise((resolve,reject) => {
+//     console.log('muz');
+//     resolve(1001);
+    
+// })
+ 
