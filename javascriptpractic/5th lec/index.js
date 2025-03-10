ae304875ed8d8a4dd9657715b842b6f2c5743947
Muzuuu <muzzus453@gmail.com>
2025-03-10 23:25:39 +0530

    one more lec done

---
+++ javascriptpractic/5th lec/index.js
@@ -0,0 +1,16 @@
+
+
+function init(){
+    // {
+    //     let name = "mak";
+    // }
+    let name ="Muzammil";
+    function displayname(){
+        // let name ="mak"; 
+        console.log(name);
+        
+    }
+    return displayname;
+}
+let inner =init();
+init();
